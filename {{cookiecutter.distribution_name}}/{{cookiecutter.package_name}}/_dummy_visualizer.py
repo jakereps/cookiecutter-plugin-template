0c38dca8018e877aeb92255c07ff78b47d5cafcc
Jorden Kreps <jakereps@users.noreply.github.com>
2016-11-02 05:18:37 -0700

    ENH: Upgrade visualizer to use q2templates (#7)

--- {{cookiecutter.distribution_name}}/{{cookiecutter.package_name}}/_dummy_visualizer.py
+++ {{cookiecutter.distribution_name}}/{{cookiecutter.package_name}}/_dummy_visualizer.py
@@ -2,8 +2,13 @@
 # ready to develop your plugin.
 import os
 import os.path
+import pkg_resources
 
 import pandas as pd
+import q2templates
+
+
+TEMPLATES = pkg_resources.resource_filename("{{cookiecutter.package_name}}", 'assets')
 
 
 def mapping_viz(output_dir: str, mapping1: dict, mapping2: dict,
@@ -11,39 +16,23 @@ def mapping_viz(output_dir: str, mapping1: dict, mapping2: dict,
     df1 = _dict_to_dataframe(mapping1, key_label, value_label)
     df2 = _dict_to_dataframe(mapping2, key_label, value_label)
 
-    with open(os.path.join(output_dir, 'index.html'), 'w') as fh:
-        fh.write('<html><head>')
-        fh.write('<link rel="stylesheet" type="text/css" '
-                 'href="css/style.css" />')
-        fh.write('</head>\n')
-        fh.write('<body>\n')
-        fh.write('<h3>mapping1:</h3>\n')
-        fh.write(df1.to_html(index=False, classes='dummy-class'))
-        fh.write('<h3>mapping2:</h3>\n')
-        fh.write(df2.to_html(index=False, classes='dummy-class'))
-        fh.write('</body></html>')
+    mapping1 = df1.to_html(index=False,
+                           classes='table table-striped table-hover')
+    mapping1 = mapping1.replace('border="1"', 'border="0"')
 
-    css_dir = os.path.join(output_dir, 'css')
-    os.mkdir(css_dir)
-    with open(os.path.join(css_dir, 'style.css'), 'w') as fh:
-        fh.write(_css)
+    mapping2 = df2.to_html(index=False,
+                           classes='table table-striped table-hover')
+    mapping2 = mapping2.replace('border="1"', 'border="0"')
+
+    index = os.path.join(TEMPLATES, 'index.html')
+    context = {
+        "mapping1": mapping1,
+        "mapping2": mapping2
+    }
+
+    q2templates.render(index, output_dir, context=context)
 
 
 def _dict_to_dataframe(dict_, key_label, value_label):
     return pd.DataFrame(sorted(dict_.items()),
                         columns=[key_label, value_label])
-
-
-# Example table styling taken from http://www.w3schools.com/css/css_table.asp
-_css = """
-.dummy-class {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-.dummy-class th, td {
-    padding: 8px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-}
-"""
